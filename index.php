e9dc5209de79fa695ef56dd4dbb5778844c67ac6
Vicente Freitas <vicenteteixeirafreitas@gmail.com>
2024-04-28 10:27:44 -0300

    Introduction to JavaScript Plugin Development

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+/*
+    Plugin Name: Are You Paying Attention Quiz
+    Description: Give your readers a multiple choice question.
+    Version: 1.0
+    Author: Vicente T F
+    Author URI: https://google.com.br
+*/
+if (!defined('ABSPATH')) exit; // Exit if accessed directly
+
+class AreYouPayingAttention
+{
+    function __construct()
+    {
+        add_action('enqueue_block_editor_assets', array($this, 'adminAssets'));
+    }
+    function adminAssets()
+    {
+        wp_enqueue_script('ournewblocktype', plugin_dir_url(__FILE__) . 'test.js', array('wp-blocks', 'wp-element'));
+    }
+}
+$areYouPayingAttention = new AreYouPayingAttention();
